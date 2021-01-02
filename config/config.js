04948f1c69e13aad38b75b2cec1fc134f490ae71
Tayfur Ünal <tunal@hurriyetemlak.com>
2021-01-02 17:22:54 +0300

    add firebase config & helpers

---
+++ config/config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBLSsmVzO4xX0x9lhbmYXovv5tXAx7rI70",
+  authDomain: "personal-library-e9533.firebaseapp.com",
+  databaseURL:
+    "https://personal-library-e9533-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "personal-library-e9533",
+  storageBucket: "personal-library-e9533.appspot.com",
+  messagingSenderId: "258764193519",
+  appId: "1:258764193519:web:fa792ebfc9643f40dfe51a",
+  measurementId: "G-S4FNGZL72C",
+};
