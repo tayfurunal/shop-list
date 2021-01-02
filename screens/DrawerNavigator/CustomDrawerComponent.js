4fffd6c9ef2f14f804273fec3f5c3c1539220714
Tayfur Ünal <tunal@hurriyetemlak.com>
2021-01-02 17:56:32 +0300

    create drawer navigator

---
+++ screens/DrawerNavigator/CustomDrawerComponent.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  Platform,
+} from "react-native";
+import colors from "../../assets/colors";
+import { Ionicons } from "@expo/vector-icons";
+
+import { DrawerItems } from "react-navigation-drawer";
+
+class CustomDrawerComponent extends React.Component {
+  render() {
+    return (
+      <ScrollView>
+        <SafeAreaView style={colors.bgMain} />
+        <View
+          style={{
+            height: 170,
+            backgroundColor: colors.bgMain,
+            alignItems: "center",
+            justifyContent: "center",
+            paddingTop: (Platform.OS = "android" ? 20 : 0),
+          }}
+        >
+          <Ionicons name="ios-bookmarks" size={100} color={colors.logoColor} />
+          <Text style={{ fontSize: 24, color: "white", fontWeight: "100" }}>
+            Menü
+          </Text>
+        </View>
+        <DrawerItems {...this.props} />
+      </ScrollView>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default CustomDrawerComponent;
