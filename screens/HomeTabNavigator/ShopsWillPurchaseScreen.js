4bf8a456e951b555504b77671ccaeb660fbf08e8
Mehmet Ali Çayhan <mehmetalicyhn@gmail.com>
2021-01-02 20:21:02 +0300

    create purchased and will purchase screens

---
+++ screens/HomeTabNavigator/ShopsWillPurchaseScreen.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { connect } from "react-redux";
+import ListItem from "../../components/ListItem";
+import colors from "../../assets/colors";
+
+class ShopsWillPurchaseScreen extends Component {
+  renderItem = (item) => {
+    return <ListItem item={item} />;
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.props.shops.shopWillPurchase}
+          renderItem={({ item }, index) => this.renderItem(item, index)}
+          keyExtractor={(item, index) => index.toString()}
+          ListEmptyComponent={
+            <View style={{ margin: 50, alignItems: "center" }}>
+              <Text style={{ fontWeight: "bold" }}>Hiç Ürün Bulumuyor</Text>
+            </View>
+          }
+        />
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    shops: state.shops,
+  };
+};
+
+export default connect(mapStateToProps)(ShopsWillPurchaseScreen);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.bgMain,
+  },
+});
