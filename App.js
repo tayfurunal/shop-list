ed6d40db4c94d1650bc31dc8d808889ccc9f4daa
Mehmet Ali Çayhan <mehmetalicyhn@gmail.com>
2021-01-02 20:29:00 +0300

    import screens to main app

--- App.js
+++ App.js
@@ -1,52 +1,190 @@
-import React from "react";
+import React, { Component } from "react";
 import { View, Text } from "react-native";
-import {
-  createAppContainer,
-  createSwitchNavigator,
-  createStackNavigator,
-  createDrawerNavigator,
-} from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
 import WelcomeScreen from "./screens/AppSwitchNavigator/WelcomeScreen";
 import { Ionicons } from "@expo/vector-icons";
 import HomeScreen from "./screens/HomeScreen";
-import SignUpScreen from "./screens/SignUpScreen";
+import LoginScreen from "./screens/LoginScreen";
 import SettingsScreen from "./screens/SettingsScreen";
-const App = () => {
-  return <AppContainer />;
+import CustomDrawerComponent from "./screens/DrawerNavigator/CustomDrawerComponent";
+import colors from "./assets/colors";
+import * as firebase from "firebase/app";
+import { firebaseConfig } from "./config/config";
+import LoadingScreen from "./screens/AppSwitchNavigator/LoadingScreen";
+import ShopsPurchasedScreen from "./screens/HomeTabNavigator/ShopsPurchasedScreen";
+import ShopsWillPurchaseScreen from "./screens/HomeTabNavigator/ShopsWillPurchaseScreen";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import ShopsCountContainer from "./redux/containers/ShopsCountContainer";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.initializeFirebase();
+  }
+  initializeFirebase = () => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+  };
+  render() {
+    return (
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    );
+  }
+}
+
+const HomeTabNavigator = createBottomTabNavigator(
+  {
+    HomeScreen: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: "Tümü",
+        tabBarIcon: ({ tintColor }) => {
+          return <ShopsCountContainer color={tintColor} type="shops" />;
+        },
+      },
+    },
+    ShopsWillPurchaseScreen: {
+      screen: ShopsWillPurchaseScreen,
+      navigationOptions: {
+        tabBarLabel: "Alacaklarım",
+        tabBarIcon: ({ tintColor }) => {
+          return (
+            <ShopsCountContainer color={tintColor} type="shopWillPurchase" />
+          );
+        },
+      },
+    },
+    ShopsPurchasedScreen: {
+      screen: ShopsPurchasedScreen,
+      navigationOptions: {
+        tabBarLabel: "Aldıklarım",
+        tabBarIcon: ({ tintColor }) => {
+          return <ShopsCountContainer color={tintColor} type="shopPurchased" />;
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      style: {
+        backgroundColor: colors.bgMain,
+      },
+      tabStyle: {
+        justifyContent: "center",
+      },
+      activeTintColor: colors.logoColor,
+      inactiveTintColor: colors.bgTextInput,
+    },
+  }
+);
+
+HomeTabNavigator.navigationOptions = ({ navigation }) => {
+  const { routeName } = navigation.state.routes[navigation.state.index];
+
+  switch (routeName) {
+    case "HomeScreen":
+      return {
+        headerTitle: "Alışveriş Listem",
+      };
+    case "ShopsWillPurchaseScreen":
+      return {
+        headerTitle: "Alacaklarım",
+      };
+    case "ShopsPurchasedScreen":
+      return {
+        headerTitle: "Aldıklarım",
+      };
+    default:
+      return {
+        headerTitle: "Alışveriş Listem",
+      };
+  }
 };
 
-const AppDrawerNavigator = createDrawerNavigator({
-  HomeScreen: {
-    screen: HomeScreen,
-    navigationOptions: {
-      title: "Home",
-      drawerIcon: () => <Ionicons name="ios-home" size={24} />,
+const HomeStackNavigator = createStackNavigator(
+  {
+    HomeTabNavigator: {
+      screen: HomeTabNavigator,
+      navigationOptions: ({ navigation }) => {
+        return {
+          headerLeft: (
+            <Ionicons
+              name="ios-menu"
+              size={30}
+              color={colors.logoColor}
+              onPress={() => navigation.openDrawer()}
+              style={{ marginLeft: 10 }}
+            />
+          ),
+        };
+      },
     },
   },
-  SettingsScreen: {
-    screen: SettingsScreen,
-    navigationOptions: {
-      title: "Settings",
-      drawerIcon: () => <Ionicons name="ios-settings" size={24} />,
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: colors.bgMain,
+      },
+      headerTintColor: colors.txtWhite,
     },
-  },
-});
+  }
+);
 
-const LoginStackNavigator = createStackNavigator({
-  WelcomeScreen: {
-    screen: WelcomeScreen,
-    navigationOptions: {
-      header: null,
+const AppDrawerNavigator = createDrawerNavigator(
+  {
+    HomeStackNavigator: {
+      screen: HomeStackNavigator,
+      navigationOptions: {
+        title: "Ana Sayfa",
+        drawerIcon: () => <Ionicons name="ios-home" size={24} />,
+      },
+    },
+    SettingsScreen: {
+      screen: SettingsScreen,
+      navigationOptions: {
+        title: "Ayarlar",
+        drawerIcon: () => <Ionicons name="ios-settings" size={24} />,
+      },
     },
   },
-  SignUpScreen: {
-    screen: SignUpScreen,
-    navigationOptions: {},
+  {
+    contentComponent: CustomDrawerComponent,
+  }
+);
+
+const LoginStackNavigator = createStackNavigator(
+  {
+    WelcomeScreen: {
+      screen: WelcomeScreen,
+      navigationOptions: {
+        header: null,
+      },
+    },
+    LoginScreen: {
+      screen: LoginScreen,
+      navigationOptions: {},
+    },
   },
-});
+  {
+    mode: "modal",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: colors.bgMain,
+      },
+    },
+  }
+);
 
 const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen,
   LoginStackNavigator,
   AppDrawerNavigator,
 });
